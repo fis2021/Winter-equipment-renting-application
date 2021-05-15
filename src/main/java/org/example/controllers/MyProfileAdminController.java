de2d2e07798713f1517a9d4516b37f1d6b1597b7
BiancaD <biancadaiana24@gmail.com>
2021-05-15 16:43:48 +0300

    WERA-55 Edit button for admin implemented

--- src/main/java/org/example/controllers/MyProfileAdminController.java
+++ src/main/java/org/example/controllers/MyProfileAdminController.java
@@ -1,7 +1,12 @@
 package org.example.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.example.Models.User;
@@ -36,6 +41,15 @@ public class MyProfileAdminController {
                 this.setEmail(user.getEmail());
                 this.setAdress(user.getAddress());
             }
+    }
+
+    @FXML
+    public void handleEditAction(javafx.event.ActionEvent edit) throws Exception{
+        Parent parentPage = FXMLLoader.load(getClass().getClassLoader().getResource("EditProfileAdminPage.fxml"));
+        Scene PageScene = new Scene(parentPage);
+        Stage window = (Stage) ((Node) edit.getSource()).getScene().getWindow();;
+        window.setScene(PageScene);
+        window.show();
     }
 
     public void setName(String name) {

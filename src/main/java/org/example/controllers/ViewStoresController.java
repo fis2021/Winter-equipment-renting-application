42e8be3ff980f5518c194fd6e535c5ce3733c07b
BiancaD <biancadaiana24@gmail.com>
2021-05-16 02:21:36 +0300

    WERA-32 View stores page created

--- src/main/java/org/example/controllers/ViewStoresController.java
+++ src/main/java/org/example/controllers/ViewStoresController.java
@@ -5,63 +5,146 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 //import jdk.vm.ci.meta.Value;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 import org.example.Models.User;
 import org.example.services.UserServices;
 //import sun.jvm.hotspot.utilities.Observable;
 
-import javax.swing.text.TabableView;
 import java.net.URL;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 public class ViewStoresController implements Initializable {
     @FXML
-    private TextField filtredField;
+    private TextField filterField;
     @FXML
     private TableView<User> tableView;
     @FXML
-    private TableColumn<User,String> name;
+    private TableColumn<User, String> name;
     @FXML
-    private TableColumn<User,String> adress;
+    private TableColumn<User, String> col_adr;
     @FXML
-    private TableColumn<User,String> email;
+    private TableColumn<User, String> email;
     @FXML
-    private TableColumn<User,String> phone;
+    private TableColumn<User, String> phone;
 
-    private final ObservableList<User> dataList= FXCollections.observableArrayList();
+    private final ObservableList<User> dataList = FXCollections.observableArrayList();
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
-        adress.setCellValueFactory(new PropertyValueFactory<>("adress"));
+        col_adr.setCellValueFactory(new PropertyValueFactory<>("address"));
         email.setCellValueFactory(new PropertyValueFactory<>("email"));
         phone.setCellValueFactory(new PropertyValueFactory<>("phone"));
 
-        for(User useri: UserServices.getUserRepository().find()){
+        for (User useri : UserServices.getUserRepository().find()) {
+            if (Objects.equals(useri.getRole(), "Admin") && !Objects.equals(useri.getName(), "")) {
 
-            dataList.add(useri);
+                User u = new User();
+                u.setName(useri.getName());
+                u.setAddress(useri.getAddress());
+                u.setEmail(useri.getEmail());
+                u.setPhone(useri.getPhone());
+                dataList.add(u);
+            }
         }
-        FilteredList<User> filteredData= new FilteredList<>(dataList, b->true);
-
-        filtredField.textProperty().addListener((observable, oldValue, newValue)-> {
+    /*    filterField.textProperty().addListener((observable, oldValue, newValue)-> {
+            filteredData.setPredicate((Predicate<? super User>) (User user) -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                else if(user.getName().contains(newValue)){
+                    return true;
+                }
+                return false;
+            });
+        });
+        SortedList<User> sort= new SortedList<>(filteredData);
+        sort.comparatorProperty().bind(tableView.comparatorProperty());
+        tableView.setItems(sort);
+*/
+      /*  filterField.textProperty().addListener((observable, oldValue, newValue)-> {
             filteredData.setPredicate(user -> {
                 if (newValue == null || newValue.isEmpty()) {
                     return true;
                 }
                 String lowerCaseFilter = newValue.toLowerCase();
-                if (user.getName().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                if (user.getName().toLowerCase().contains(lowerCaseFilter)) {
                     return true;
                 } else return false;
             } );
             } );
         SortedList<User> sortedData= new SortedList<>(filteredData);
-        sortedData.comparatorProperty().bind(tableView.comparatorProperty());
-        tableView.setItems(sortedData);
+        sortedData.comparatorProperty().bind(tableView.comparatorProperty());*/
+        tableView.setItems(dataList);
+        //tableView.setItems(sortedData);
+
+
+
+
+        //@FXML
+   /* public void search(javafx.scene.input.KeyEvent keyEvent) {
+        filterField.textProperty().addListener((observable, oldValue, newValue)-> {
+            filteredData.setPredicate((Predicate<? super User>) (User user) -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                else if(user.getName().contains(newValue)){
+                    return true;
+                }
+                return false;
+            });
+        });
+        SortedList sort= new SortedList(filteredData);
+        sort.comparatorProperty().bind(tableView.comparatorProperty());
+        tableView.setItems(sort);
+    }*/
+
+
+
+    }
+    FilteredList<User> filteredData = new FilteredList<>(dataList, b -> true);
+    public void search(KeyEvent keyEvent) {
+
+            filterField.textProperty().addListener((observable, oldValue, newValue)-> {
+                filteredData.setPredicate((Predicate<? super User>) (User user) -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String lowerCaseFilter= newValue.toLowerCase();
+                    if(user.getName().toLowerCase().contains(lowerCaseFilter)){
+                        return true;
+                    }
+                    return false;
+                });
+            });
+            SortedList sort= new SortedList(filteredData);
+            sort.comparatorProperty().bind(tableView.comparatorProperty());
+            tableView.setItems(sort);
+
+    }
+
+    @FXML
+    public void HomeAction(javafx.event.ActionEvent home) throws Exception{
+        Parent parentPage = FXMLLoader.load(getClass().getClassLoader().getResource("HomePage.fxml"));
+        Scene PageScene = new Scene(parentPage);
+        Stage window = (Stage) ((Node) home.getSource()).getScene().getWindow();;
+        window.setScene(PageScene);
+        window.show();
     }
 
 }
+

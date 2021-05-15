492913da49ebada225ec6fc63ab67498cb1d79de
Alexandra Carabas <alexandra.carabas@student.upt.ro>
2021-05-15 14:53:58 +0300

    WERA-54 My profile button for Admin

--- src/main/java/org/example/controllers/MyProfileAdminController.java
+++ src/main/java/org/example/controllers/MyProfileAdminController.java
@@ -2,6 +2,14 @@ package org.example.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.example.Models.User;
+import org.example.services.UserServices;
+
+import java.util.Objects;
+
+import static org.example.services.FileSystemServices.getPathToFile;
 
 public class MyProfileAdminController {
     @FXML
@@ -13,32 +21,49 @@ public class MyProfileAdminController {
     @FXML
     private Text adress;
 
-    public void setName(Text name) {
-        this.name = name;
+    private static ObjectRepository<User> userRep;
+    private static String loggedUser;
+
+    private static Nitrite database;
+
+    public void set(){
+        loggedUser=LogInController.getLoggedUser();
+        userRep = UserServices.getUserRepository();
+        for(User user:userRep.find())
+            if(Objects.equals(loggedUser,user.getUsername())){
+                this.setName(user.getName());
+                this.setPhone(user.getPhone());
+                this.setEmail(user.getEmail());
+                this.setAdress(user.getAddress());
+            }
+    }
+
+    public void setName(String name) {
+        this.name.setText(name);
     }
 
     public Text getName() {
         return name;
     }
 
-    public void setPhone(Text phone) {
-        this.phone = phone;
+    public void setPhone(String phone) {
+        this.phone.setText(phone);
     }
 
     public Text getPhone() {
         return phone;
     }
 
-    public void setEmail(Text email) {
-        this.email = email;
+    public void setEmail(String email) {
+        this.email.setText(email);
     }
 
     public Text getEmail() {
         return email;
     }
 
-    public void setAdress(Text adress) {
-        this.adress = adress;
+    public void setAdress(String adress) {
+        this.adress.setText(adress);
     }
 
     public Text getAdress() {

c2730deb392ee695a8808a4b7b8a988125f0882a
Alexandra Carabas <alexandra.carabas@student.upt.ro>
2021-05-16 17:10:34 +0300

    WERA-29 connection between Store Page and View Stores Page

--- src/main/java/org/example/controllers/StorePageController.java
+++ src/main/java/org/example/controllers/StorePageController.java
@@ -1,4 +1,19 @@
 package org.example.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class StorePageController {
+    @FXML
+    public void BackAction(javafx.event.ActionEvent back) throws Exception{
+        Parent parentPage = FXMLLoader.load(getClass().getClassLoader().getResource("ViewStoresPage.fxml"));
+        Scene PageScene = new Scene(parentPage);
+        Stage window = (Stage) ((Node) back.getSource()).getScene().getWindow();
+        window.setScene(PageScene);
+        window.show();
+    }
 }
